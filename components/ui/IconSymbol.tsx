06c9363a156a03d5ea9fea2c11a5f99f5e5ff0f2
NathanSemet <220674@umons.ac.be>
2026-05-18 19:42:07 +0200

    Correction of small bugs on the base code

--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -13,12 +13,7 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
-} as Partial<
-  Record<
-    import('expo-symbols').SymbolViewProps['name'],
-    React.ComponentProps<typeof MaterialIcons>['name']
-  >
->;
+} as Partial<Record<string, React.ComponentProps<typeof MaterialIcons>['name']>>;
 
 export type IconSymbolName = keyof typeof MAPPING;
 
@@ -39,5 +34,5 @@ export function IconSymbol({
   style?: StyleProp<ViewStyle>;
   weight?: SymbolWeight;
 }) {
-  return <MaterialIcons color={color} size={size} name={MAPPING[name]} style={style} />;
+  return <MaterialIcons color={color} size={size} name={MAPPING[name]} />;
 }
